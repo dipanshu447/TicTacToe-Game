e8dada3181e73c73c9f5cc8870452b0334bdc12c
dipanshu447 <dipanshusahu447@gmail.com>
2025-03-19 15:40:45 +0530

    Added player/winner announcer element logics

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var winCombo = [
     [0, 3, 6], [1, 4, 7], [2, 5, 8], // columns
     [0, 4, 8], [2, 4, 6] // diagonals
 ];
+var annoncer = document.getElementsByTagName("header")[0];
 
 var checkWinner = (player) => {
     for (let i = 0; i < winCombo.length; i++) {
@@ -16,29 +17,48 @@ var checkWinner = (player) => {
     return false;
 }
 
+let gameOver = false;
+var makeMove = (index, player) => {
+    if (board[index] === "" && !gameOver) {
+        board[index] = player;
+        if (checkWinner(player)) {
+            annoncer.innerText = `Player ${player} Won`;
+            gameOver = true;
+        } else if (!board.includes("")) {
+            gameOver = true;
+            annoncer.innerText = "It's a Tie";
+        }
+    }
+}
+
 // swtiching between X and O
 let turn = true;
 let player = "";
 boxes.forEach((e) => {
     e.addEventListener("click", () => {
+        if (e.children.length > 0) return;
         let index = e.dataset.index;
-        if(board[index] !== "") return;
+        if (board[index] !== "") return;
 
         let XO = document.createElement('img');
-
-        if (turn) {
-            player = "X";
-            XO.src = 'https://img.icons8.com/?size=100&id=pNXET7bXhanM&format=png&color=000000';
-            XO.alt = 'X-img';
-            board[index] = player;
-        } else {
-            player = "O";
-            XO.src = 'https://img.icons8.com/?size=100&id=18722&format=png&color=8a2be2';
-            XO.alt = 'circle-img';
-            board[index] = player;
+        if (!gameOver) {
+            if (turn) {
+                player = "X";
+                XO.src = 'https://img.icons8.com/?size=100&id=pNXET7bXhanM&format=png&color=000000';
+                XO.alt = 'X-img';
+                annoncer.innerText = "Player O's Turn";
+                makeMove(index, player);
+            } else {
+                player = "O";
+                XO.src = 'https://img.icons8.com/?size=100&id=18722&format=png&color=8a2be2';
+                XO.alt = 'circle-img';
+                annoncer.innerText = "Player X's Turn";
+                makeMove(index, player);
+            }
+            e.appendChild(XO);
+            turn = !turn;
         }
-        e.appendChild(XO);
-        turn = !turn;
+
         console.log(board);
         console.log(checkWinner(player));
     })
